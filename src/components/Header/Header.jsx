73efca4232b6f0733458547c146caca105b6f926
ArnabNayan <arnabnayan5@gmail.com>
2023-03-27 22:52:16 +0600

    added products

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,12 @@ import logo from'../../images/Logo.svg';
 const Header = () => {
     return (
         <nav className='header'>
-           <img src={logo} alt="" srcset="" /> 
+          <img src={logo} alt="" /> 
           <div>
-          <a href="/shop">shop</a>
-           <a href="/order">order</a>
-           <a href="/inventory">inventory</a>
-           <a href="/login">login</a>
+          <a href="/shop">Shop</a>
+           <a href="/order">Order</a>
+           <a href="/inventory">Inventory</a>
+           <a href="/login">Login</a>
           </div>
         </nav>
     );
